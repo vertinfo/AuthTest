4399c08ffed2c1c26da71811893567ec6ce86c17
Dan Bryant <dbryant@ii4tech.com>
2021-10-14 15:28:59 -0500

    Baby Steps

---
+++ AuthTest/Models/Login.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuthTest.Models
+{
+    public class Login
+    {
+        public string user { get; set; }
+        public string password { get; set; }
+    }
+}
